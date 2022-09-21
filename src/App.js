1cc949eb0cfb5174a6b74fb1f88ba9024c94e13e
ACwolf55 <66705643+ACwolf55@users.noreply.github.com>
2022-09-21 13:48:10 -0700

    *

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
+import React,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Comp from './Comp';
+import Loading from './Loading';
 
 function App() {
+
+
+  const [num ,setNum ] = useState(1)
+
+  const [username,setUsername] = useState('falcon')
+
+  const [food, setfood] = useState('cake')
+
+  const [car, setcar] = useState('corvette')
+
+  const [arr,setarr] = useState([0,1,2,3,4])
+
+
+  const math =()=>{
+    let newArr = []
+    for(let i =0 ; i<arr.length;i++){
+      newArr.push(arr[i]++)
+    }
+    setarr(newArr)
+    console.log(arr)
+  }
+
+  const increment =()=>{
+   setNum((prevState)=>prevState+1)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <p>Hello World!~!~</p>
+    <p>8*8</p>
+    <p>{8*8}</p>
+    <p>{num}</p>
+    <p>{food}</p>
+    <p>{car}</p>
+    <button onClick={increment}>+</button>
+    <div className='math' onClick={math}>
+      <h4>math is ok i guess</h4>
+      <h5>~~~~~</h5>
+    </div>
+
+    <button onClick={()=>console.log(arr)}>console log array!</button>
+    <input onChange={(e)=>setUsername(e.target.value)}></input>
+    <button onClick={()=>setcar('scion')}>corvette to scion</button>
+     <Comp />
+    <h3>{username}</h3>
+
+
+     <Comp />
+
+
+    <footer>
+      <Loading />
+    </footer>
     </div>
   );
 }
